c1aae383e5693a17993d80af77f2c5e7ef4ad440
Libor Váchal <libor.vachal@gmail.com>
2016-11-29 19:54:09 +0100

    init project

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Dozuki import</title>
+        
+    </head>
+    <body>
+        <h1>Dozuki import</h1>>
+        <?php
+            require_once('./import.php'); 
+        ?>
+    </body>
+</html>
